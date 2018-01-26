c36ca040e8be6b2da0856acdfe0b642e9550b430
GeorgeDonev <georgesdonev@gmail.com>
2018-01-25 20:27:22 -1000

    test

---
+++ ch3-exc1.js
@@ -0,0 +1,48 @@
+
+//bad god functions, difficult to expand more items (types of animals)
+
+function printFarmInventory(cows, chickens) {
+    var cowString = String(cows);
+    while (cowString.length < 3 )
+        cowString = "0" + cowString;
+    console.log(cowString + " Cows");
+
+    var chickenString = String(chickens);
+    while (chickenString.length < 3 )
+        chickenString = "0" + chickenString;
+    console.log(chickenString + " Chickens")
+}
+printFarmInventory(7,11);
+console.log( "" );
+
+
+//better way of approaching a growing function
+function printZeroPaddedWithLabel(number, label) {
+    var numberString = String(number);
+    while (numberString.length < 3 )
+        numberString = "0" + numberString;
+    console.log(numberString + " " + label);
+}
+function printFarmInventory(cows, chickens, pigs){
+    printZeroPaddedWithLabel(cows, "Cows");
+    printZeroPaddedWithLabel(chickens, "Chickens");
+    printZeroPaddedWithLabel(pigs, "Pigs");
+}
+printFarmInventory(7,11,3);
+
+console.log("")
+
+//more singular conceptual approach to creating functions1234
+
+function zeroPad(number, width) {
+    var string = String(number);
+    while (string.length < width)
+        string = "0" + string;
+    return string;
+}
+function printFarmInventory (cows, chickens, pigs) {
+    console.log(zeroPad(cows, 3) + " Cows");
+    console.log(zeroPad(chickens, 3) + " Chickens");
+    console.log(zeroPad(pigs, 3) + " Pigs");
+}
+printFarmInventory(7, 16, 3);
---
+++ inflation_test.js
@@ -0,0 +1,15 @@
+//this is poopy right now, come back to it later, you are juggling too many things at once
+function desiredTime(years, priciple) {
+   // var equation = priciple * ();
+}
+
+function accumulation(time, nominalAnnualInterestRate){
+    return 1 + (time * nominalAnnualInterestRate)
+}
+
+
+var masterEquation = accumulation() * (1 + ((nominalAnnualInterestRate / (time * periods)  ) ^ floor(time * periods)) )
+
+function masterCalculator(time, nominalAnnualInterestRate, ) {
+
+}
